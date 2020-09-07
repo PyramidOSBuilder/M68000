29d3315fa5a4363f2c47486c4d3447a26f1a48d1
Romulus <romulus@PC6502.romae>
2020-09-06 22:24:55 -0400

    Added hexdump util

---
+++ utils/src/hex.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc,char** argv){
+	if(argc < 2){
+		printf("Usage: hex [in]\n");
+		return 0;
+	}
+	
+	FILE* fp = fopen(argv[1],"rb");
+	if(fp == NULL){
+		printf("Error opening %s\n",argv[1]);
+		return 0;
+	}
+	
+	int c = 0;
+	while((c = fgetc(fp)) != EOF)
+		printf("%x ",c);
+	
+	fclose(fp);
+	
+	return 0;
+	
+}
